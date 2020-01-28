0d994ca19f7580b14e4d1354769a184152c1846c
Mehmet Uluer <mehmet.uluer@gmail.com>
2020-01-29 00:29:18 +0300

    exception

---
+++ parent/exception/src/main/java/com/timeith/exception/NewsErrorDefinition.java
@@ -0,0 +1,11 @@
+package com.timeith.exception;
+
+public class NewsErrorDefinition {
+	public final static String NEWS_CODE_PREFIX = "01";
+	
+	public final static String DB_CODE_PREFIX = NEWS_CODE_PREFIX + "01";
+	
+	public final static String DB_REQUEST_NOT_FOUND = DB_CODE_PREFIX + "01";
+	
+
+}
---
+++ parent/exception/src/main/java/com/timeith/exception/NewsException.java
@@ -0,0 +1,9 @@
+package com.timeith.exception;
+
+public class NewsException extends Exception {
+
+	private static final long serialVersionUID = 7484000374826667095L;
+	
+	
+
+}

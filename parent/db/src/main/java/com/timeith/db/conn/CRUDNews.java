2dbdbcc11ef1633ed82718dacf9cb6891766c33a
Mehmet Uluer <mehmet.uluer@gmail.com>
2020-01-29 00:28:02 +0300

    create

--- parent/db/src/main/java/com/timeith/db/conn/CRUDNews.java
+++ parent/db/src/main/java/com/timeith/db/conn/CRUDNews.java
@@ -4,6 +4,7 @@ import java.time.ZonedDateTime;
 import java.util.List;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 import com.timeith.models.NewsHMDL;
@@ -11,48 +12,61 @@ import com.timeith.models.NewsHMDL;
 @SuppressWarnings("unchecked")
 public class CRUDNews {
 
-	public static void create(Session session, int count) throws Exception {
+	public static NewsHMDL create(int count) throws Exception {
 		NewsHMDL newsItem = new NewsHMDL();
 		newsItem.setTitle("test title" + count);
 		newsItem.setDescription("test description" + count);
 		newsItem.setPublishDate(ZonedDateTime.now());
-		long newsId = -1L;
-		newsId = (long) session.save(newsItem);
-		if (newsId == -1)
-			throw new Exception("Create news item failed..");
-		System.out.println(newsId + " created..");
+		Session session = HibernateUtils.getSession();
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			session.save(newsItem);
+			transaction.commit();
+			return newsItem;
+		} catch (Exception e) {
+			if (transaction != null)
+				transaction.rollback();
+			throw new Exception("Create news item failed..", e.getCause());
+		} finally {
+			session.close();
+		}
 	}
 
-	public static void readAll(Session session) throws Exception {
+	public static List<NewsHMDL> readAll(Session session) throws Exception {
 		String sql = "SELECT n FROM " + NewsHMDL.class.getName() + " n " + "ORDER BY n.newsId";
 		Query<NewsHMDL> query = null;
 		query = session.createQuery(sql);
 		List<NewsHMDL> newsList = query.getResultList();
 		if (newsList.isEmpty())
 			throw new Exception("News Table is empty..");
-		for (NewsHMDL newsHMDL : newsList) {
-			System.out.println(newsHMDL.getNewsId() + "\t" + newsHMDL.getTitle() + "\t" + newsHMDL.getDescription()
-					+ "\t" + newsHMDL.getPublishDate());
-		}
+		return newsList;
 	}
 
-	public static void update(Session session, long newsId) throws Exception {
+	public static NewsHMDL readSingle(Session session, long newsId) throws Exception {
+		NewsHMDL newsHMDL = session.find(NewsHMDL.class, newsId);
+		if (newsHMDL == null)
+			throw new Exception("News Item not found..");
+		return newsHMDL;
+	}
+
+	public static NewsHMDL update(Session session, long newsId) throws Exception {
 		NewsHMDL newsItem = null;
 		newsItem = session.get(NewsHMDL.class, newsId);
 		if (newsItem == null)
 			throw new Exception("News Id not found for Update..");
-		newsItem.setPublishDate(ZonedDateTime.now());
+		newsItem.setPublishDate(ZonedDateTime.now().minusDays(1));
 		session.update(newsItem);
-		System.out.println("News item Updated: " + newsItem.toString());
+		return newsItem;
 	}
 
-	public static void delete(Session session, long newsId) throws Exception {
+	public static Boolean delete(Session session, long newsId) throws Exception {
 		NewsHMDL newsItem = null;
 		newsItem = session.get(NewsHMDL.class, newsId);
 		if (newsItem == null)
 			throw new Exception("News Id not found to delete..");
 		session.delete(newsItem);
-		System.out.println("News item deleted: " + newsItem.toString());
+		return true;
 	}
 
 }

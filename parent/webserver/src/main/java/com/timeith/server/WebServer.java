ff707f1bc80c15cf0bd862b1cbb5279b2a8ebb6c
Mehmet Uluer <mehmet.uluer@gmail.com>
2020-01-29 00:25:21 +0300

    ResourceConfig

--- parent/webserver/src/main/java/com/timeith/server/WebServer.java
+++ parent/webserver/src/main/java/com/timeith/server/WebServer.java
@@ -1,36 +1,48 @@
 package com.timeith.server;
 
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletHandler;
-
-import com.timeith.webapi.HelloServlet;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
 
 /**
  * Web Server
  *
  */
-public class WebServer 
-{
+public class WebServer {
 	public static Server createServer(int port) {
-		Server server= new Server(port);
-		ServletHandler servletHandler = new ServletHandler();
-		server.setHandler(servletHandler);
-		servletHandler.addServletWithMapping(HelloServlet.class, "/*");
-		return server;
+		Server jettyServer = new Server();
+		ServerConnector jettyServerConnector = new ServerConnector(jettyServer);
+		jettyServerConnector.setPort(port);
+		jettyServerConnector.setIdleTimeout(300000);
+		jettyServer.addConnector(jettyServerConnector);
 		
+		
+		 jerseyResourceConfig = new ResourceConfig();
+		jerseyResourceConfig.packages("com.timeith.webapi.services");
+		ServletContainer jerseyServletContainer = new ServletContainer(jerseyResourceConfig);
+		ServletHolder webapiServletHolder = new ServletHolder(jerseyServletContainer);
+		
+		ServletContextHandler jettyServletContextHandler = new ServletContextHandler();
+		jettyServletContextHandler.setContextPath("/webapi/v1");
+		jettyServletContextHandler.addServlet(webapiServletHolder, "/*");
+		jettyServer.setHandler(jettyServletContextHandler);
+		
+		return jettyServer;
+
 	}
-	
-    public static void main( String[] args )
-    {
-    	int port = 9090;
-    	Server server = createServer(port);
-    	try {
+
+	public static void main(String[] args) {
+		int port = 9090;
+		Server server = createServer(port);
+		try {
 			server.start();
-	        server.dumpStdErr();
+			//server.dumpStdErr();
 			server.join();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+	}
 }

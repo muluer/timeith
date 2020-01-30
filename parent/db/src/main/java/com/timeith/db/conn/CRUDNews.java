1cfe4b55a44d2364fa3016a7a044de671a26afbe
Mehmet Uluer <mehmet.uluer@gmail.com>
2020-01-30 10:18:24 +0300

    session and transaction

--- parent/db/src/main/java/com/timeith/db/conn/CRUDNews.java
+++ parent/db/src/main/java/com/timeith/db/conn/CRUDNews.java
@@ -19,11 +19,16 @@ public class CRUDNews {
 		newsItem.setPublishDate(ZonedDateTime.now());
 		Session session = HibernateUtils.getSession();
 		Transaction transaction = null;
+		long newsId = -1;
 		try {
 			transaction = session.beginTransaction();
-			session.save(newsItem);
-			transaction.commit();
-			return newsItem;
+			newsId = (long) session.save(newsItem);
+			if (newsId == -1) {
+				throw new Exception("News ID not generated..");
+			} else {
+				transaction.commit();
+				return newsItem;
+			}
 		} catch (Exception e) {
 			if (transaction != null)
 				transaction.rollback();
@@ -33,40 +38,120 @@ public class CRUDNews {
 		}
 	}
 
-	public static List<NewsHMDL> readAll(Session session) throws Exception {
-		String sql = "SELECT n FROM " + NewsHMDL.class.getName() + " n " + "ORDER BY n.newsId";
+	public static List<NewsHMDL> readAll() throws Exception {
+		Session session = HibernateUtils.getSession();
+		Transaction transaction = null;
 		Query<NewsHMDL> query = null;
-		query = session.createQuery(sql);
-		List<NewsHMDL> newsList = query.getResultList();
-		if (newsList.isEmpty())
-			throw new Exception("News Table is empty..");
-		return newsList;
+		try {
+			transaction = session.beginTransaction();
+			String sql = "SELECT n FROM " + NewsHMDL.class.getName() + " n " + "ORDER BY n.newsId";
+			query = session.createQuery(sql);
+			List<NewsHMDL> newsList = query.getResultList();
+			if (newsList.isEmpty())
+				throw new Exception("News Table is empty..");
+			else {
+				transaction.commit();
+				return newsList;
+			}
+		} catch (Exception e) {
+			if (transaction != null)
+				transaction.rollback();
+			throw new Exception("Read all news item failed..", e.getCause());
+		} finally {
+			session.close();
+		}
 	}
 
-	public static NewsHMDL readSingle(Session session, long newsId) throws Exception {
-		NewsHMDL newsHMDL = session.find(NewsHMDL.class, newsId);
-		if (newsHMDL == null)
-			throw new Exception("News Item not found..");
-		return newsHMDL;
+	public static NewsHMDL readSingle(long newsId) throws Exception {
+		Session session = HibernateUtils.getSession();
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			NewsHMDL newsHMDL = session.find(NewsHMDL.class, newsId);
+			if (newsHMDL == null)
+				throw new Exception("News Item not found..");
+			else {
+				transaction.commit();
+				return newsHMDL;
+			}
+		} catch (Exception e) {
+			if (transaction != null)
+				transaction.rollback();
+			throw new Exception("Read single news item failed..", e.getCause());
+		} finally {
+			session.close();
+		}
 	}
 
-	public static NewsHMDL update(Session session, long newsId) throws Exception {
+	public static NewsHMDL update(long newsId) throws Exception {
+		Session session = HibernateUtils.getSession();
+		Transaction transaction = null;
 		NewsHMDL newsItem = null;
-		newsItem = session.get(NewsHMDL.class, newsId);
-		if (newsItem == null)
-			throw new Exception("News Id not found for Update..");
-		newsItem.setPublishDate(ZonedDateTime.now().minusDays(1));
-		session.update(newsItem);
-		return newsItem;
+		try {
+			transaction = session.beginTransaction();
+			newsItem = session.get(NewsHMDL.class, newsId);
+			if (newsItem == null)
+				throw new Exception("News Id not found for Update..");
+			else {
+				newsItem.setPublishDate(ZonedDateTime.now().minusDays(1));
+				session.update(newsItem);
+				transaction.commit();
+				return newsItem;
+			}
+		} catch (Exception e) {
+			if (transaction != null)
+				transaction.rollback();
+			throw new Exception("Update news item failed..", e.getCause());
+		} finally {
+			session.close();
+		}
 	}
 
-	public static Boolean delete(Session session, long newsId) throws Exception {
+	public static Boolean delete(long newsId) throws Exception {
+		Session session = HibernateUtils.getSession();
+		Transaction transaction = null;
 		NewsHMDL newsItem = null;
-		newsItem = session.get(NewsHMDL.class, newsId);
-		if (newsItem == null)
-			throw new Exception("News Id not found to delete..");
-		session.delete(newsItem);
-		return true;
+		try {
+			transaction = session.beginTransaction();
+			newsItem = session.get(NewsHMDL.class, newsId);
+			if (newsItem == null)
+				throw new Exception("News Id not found to delete..");
+			else {
+				session.delete(newsItem);
+				transaction.commit();
+				return true;
+			}
+		} catch (Exception e) {
+			if (transaction != null)
+				transaction.rollback();
+			throw new Exception("Delete news item failed..", e.getCause());
+		} finally {
+			session.close();
+		}
+	}
+
+	public static int deleteAll() throws Exception {
+		Session session = HibernateUtils.getSession();
+		Transaction transaction = null;
+		Query<NewsHMDL> query = null;
+		try {
+			transaction = session.beginTransaction();
+			String sql = "DELETE FROM " + NewsHMDL.class.getName();
+			query = session.createQuery(sql);
+			int itemCount = query.executeUpdate();
+			if (itemCount == 0)
+				throw new Exception("Table is empty..");
+			else {
+				transaction.commit();
+				return itemCount;
+			}
+		} catch (Exception e) {
+			if (transaction != null)
+				transaction.rollback();
+			throw new Exception("Delete all news failed..", e.getCause());
+		} finally {
+			session.close();
+		}
 	}
 
 }

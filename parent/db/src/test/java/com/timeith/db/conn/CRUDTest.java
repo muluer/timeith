fddab5875ed119bf33aafba68855c987e5c54cce
Mehmet Uluer <mehmet.uluer@gmail.com>
2020-01-30 10:18:49 +0300

    move session to backend

--- parent/db/src/test/java/com/timeith/db/conn/CRUDTest.java
+++ parent/db/src/test/java/com/timeith/db/conn/CRUDTest.java
@@ -2,13 +2,9 @@ package com.timeith.db.conn;
 
 import java.util.List;
 
-import org.hibernate.Session;
-
 import com.timeith.models.NewsHMDL;
 
 public class CRUDTest {
-
-	private static Session session = null;
 
 	public static void main(String[] args) {
 		
@@ -17,41 +13,35 @@ public class CRUDTest {
 		int count = 45;
 
 		try {
-			session = HibernateUtils.getSession();
 			System.out.println("Test news item create..");
 			newsItem = CRUDNews.create(count);
 			System.out.println("News item created: " + newsItem.toString());
 			HibernateUtils.closeSession();
 
-			session = HibernateUtils.getSession();
 			System.out.println("Test read all news item..");
-			List<NewsHMDL> newsList = CRUDNews.readAll(session);
+			List<NewsHMDL> newsList = CRUDNews.readAll();
 			for (NewsHMDL newsHMDL : newsList)
 				System.out.println(newsHMDL.toString());
 			HibernateUtils.closeSession();
 
-			session = HibernateUtils.getSession();
 			System.out.println("Test update news item..");
-			newsItem = CRUDNews.update(session, newsId);
+			newsItem = CRUDNews.update(newsId);
 			System.out.println("News item updated: " + newsItem.toString());
 			HibernateUtils.closeSession();
 
-			session = HibernateUtils.getSession();
 			System.out.println("Test read single news item..");
-			newsItem = CRUDNews.readSingle(session, newsId);
+			newsItem = CRUDNews.readSingle(newsId);
 			System.out.println("News item read: " + newsItem.toString());
 			HibernateUtils.closeSession();
 
-			session = HibernateUtils.getSession();
 			System.out.println("Test delete news item..");
-			Boolean isDeleted = CRUDNews.delete(session, newsId);
+			Boolean isDeleted = CRUDNews.delete(newsId);
 			if (isDeleted)
 				System.out.println("News item deleted: " + newsId + " deleted..");
 			HibernateUtils.closeSession();
 
-			session = HibernateUtils.getSession();
 			System.out.println("Test read all news item..");
-			CRUDNews.readAll(session);
+			CRUDNews.readAll();
 			for (NewsHMDL newsHMDL : newsList)
 				System.out.println(newsHMDL.toString());
 			HibernateUtils.closeSession();

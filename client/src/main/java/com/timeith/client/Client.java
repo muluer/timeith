457cce7e7937dd6c448a4a67a0e5a57b71019681
Mehmet Uluer <mehmet.uluer@gmail.com>
2020-02-01 14:41:07 +0300

    work tree structure

---
+++ client/src/main/java/com/timeith/client/Client.java
@@ -0,0 +1,13 @@
+package com.timeith.client;
+
+/**
+ * Timeith Client
+ *
+ */
+public class Client 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

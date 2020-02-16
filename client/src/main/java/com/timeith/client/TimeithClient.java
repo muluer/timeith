fb1d145e9167d002d2e7402767539415b5efd2dc
Mehmet Uluer <mehmet.uluer@gmail.com>
2020-02-16 10:42:30 +0300

    timeith client

--- client/src/main/java/com/timeith/client/TimeithClient.java
+++ client/src/main/java/com/timeith/client/TimeithClient.java
@@ -1,13 +1,27 @@
 package com.timeith.client;
 
+import java.io.InputStream;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
+
 /**
  * Timeith Client
  *
  */
-public class Client 
+public class TimeithClient 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	private Client client = ClientBuilder.newClient();
+
+	public InputStream getRSS(String content_uri) {
+		Response response = client
+				.target(content_uri)
+				.path("/")
+				.request()
+				.get();
+		InputStream contentBody = response.readEntity(InputStream.class);
+		return contentBody;
+	}
+	
 }

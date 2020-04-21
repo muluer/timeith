e38cddac13a523a32865a9001551ac1f9bedf36c
Mehmet Uluer <mehmet.uluer@gmail.com>
2020-04-21 07:16:21 +0300

    covid19

--- driver/src/test/java/com/timeith/driver/CovidAnalysis.java
+++ driver/src/test/java/com/timeith/driver/CovidAnalysis.java
@@ -6,10 +6,12 @@ import static org.apache.spark.sql.functions.desc;
 import static org.apache.spark.sql.functions.explode;
 import static org.apache.spark.sql.functions.row_number;
 import static org.apache.spark.sql.functions.sum;
+import static org.apache.spark.sql.functions.round;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.expressions.Window;
 import org.apache.spark.sql.types.DataTypes;
@@ -17,45 +19,80 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * covid analysis
+ * covid19 analysis
  *
  */
-public class CovidAnalysis
-{
+public class CovidAnalysis {
 	private static final Logger LOGGER = LoggerFactory.getLogger(CovidAnalysis.class);
-    public static void main( String[] args )
-    {
-        LOGGER.info("starting driver.." );
-        SparkConf conf = new SparkConf()
-        		.setAppName("Covid19")
-        		.setMaster("local[3]");
-        		
-        SparkSession spark= SparkSession
-        		.builder()
-        		.config(conf)
-        		.getOrCreate();
+	private static Dataset<Row> sortedDeathRecords, sortedCasesRecords, sortedPecentageRecords, worldTotal;	
+
+
+	public static void main(String[] args) {
+		
+		LOGGER.info("starting driver..");
+		SparkSession spark= SparkDriver.getOrCreate();
+		
+		String path = "src/main/resources/";
+		String datasetFile = path + "COVID19_20200418.json";
         
-        String path = "src/main/resources/COVID19_20200412.json";
         Dataset<Row> covidDataset = spark
         		.read()
         		.option("multiline", true)
-        		.json(path);
+        		.json(datasetFile);
       
-        Dataset<Row> sortedRecords = covidDataset
-        		.select(explode(col("records")).alias("total"))
-        		.withColumn("country", col("total").getField("countriesAndTerritories"))
-        		.withColumn("cases", col("total").getField("cases").cast(DataTypes.IntegerType))
-				.withColumn("deaths", col("total").getField("deaths").cast(DataTypes.IntegerType))
+        Dataset<Row> groupedRecords = covidDataset
+        		.select(explode(col("records")).alias("country total"))
+        		.withColumn("country", col("country total").getField("countriesAndTerritories"))
+        		.withColumn("cases", col("country total").getField("cases").cast(DataTypes.IntegerType))
+				.withColumn("deaths", col("country total").getField("deaths").cast(DataTypes.IntegerType))
 				.groupBy("country")
-				.agg(sum("cases").alias("total cases"), sum("deaths").alias("total deaths"))
-				.withColumn("idx", row_number().over(Window.orderBy(desc("total deaths"))))
+				//.map
+				.agg(sum("cases").alias("country total cases"), sum("deaths").alias("country total deaths"))
+        		.withColumn("percent", round(col("country total deaths").divide(col("country total cases")), 2))
 				.cache();
         
-        Dataset<Row> indexedRecords = sortedRecords.select(col("idx"), col("country"), col("total cases"), col("total deaths"));
-        indexedRecords.printSchema();
-        indexedRecords.show(20, false);
-     
+        sortedDeathRecords = groupedRecords
+        		.withColumn("idx (sorted deaths)", row_number().over(Window.orderBy(desc("country total deaths"))))
+        		.select(col("idx (sorted deaths)"), col("country"), col("country total cases"), col("country total deaths"), col("percent"));
+
+        sortedCasesRecords = groupedRecords
+        		.withColumn("idx (sorted cases)", row_number().over(Window.orderBy(desc("country total cases"))))
+        		.select(col("idx (sorted cases)"), col("country"), col("country total cases"), col("country total deaths"), col("percent"));
+
+        sortedPecentageRecords = groupedRecords
+        		.withColumn("idx (sorted percent)", row_number().over(Window.orderBy(desc("percent"))))
+        		.select(col("idx (sorted percent)"), col("country"), col("country total cases"), col("country total deaths"), col("percent"));
+
+        worldTotal = groupedRecords
+        		.agg(sum(col("country total cases")).alias("world total cases"), sum(col("country total deaths")).alias("world total deaths"))
+        		.select(col("world total cases"), col("world total deaths"), round(col("world total deaths").divide(col("world total cases")), 2).alias("percent"));
+
+        sortedDeathRecords.write().format("csv").mode(SaveMode.Overwrite).save(path + "sortedDeathRecords.csv");
+        sortedCasesRecords.write().format("csv").mode(SaveMode.Overwrite).save(path + "sortedCasesRecords.csv");
+        sortedPecentageRecords.write().format("csv").mode(SaveMode.Overwrite).save(path + "sortedPecentageRecords.csv");
+        worldTotal.write().format("csv").mode(SaveMode.Overwrite).save(path + "worldTotal.csv");
+
+        int count = (int) groupedRecords.count();
+        print(count);
+
         LOGGER.info("stopping driver.." );
         spark.close();
     }
+
+    private static void print(int count) {
+		sortedDeathRecords.show(count, false);
+        sortedCasesRecords.show(count, false);
+        sortedPecentageRecords.show(count, false);
+        sortedPecentageRecords
+        		.filter(sortedPecentageRecords.col("country total cases").gt(250))
+        		.filter(sortedPecentageRecords.col("country total deaths").gt(10))
+				.select(row_number().over(Window.orderBy(desc("percent"))).alias("idx (filtered)"), 
+						col("country"),
+						col("country total cases"), 
+						col("country total deaths"), 
+						col("percent"))
+        		.show(count, false);        		
+
+        worldTotal.show(false);	
+	}
 }
